33c9f60fad6a3d925f5764314b5bb656eb37c72e
VadimTroyan <vadimtroyan91@gmail.com>
2020-05-21 17:22:20 +0300

    new commit

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 function addTask(text) {
-   var ul = document.getElementById('task-list');
-   var li = document.createElement('li');
-   li.innerHTML = text + '<button>done</button>\n' +
-       '<button>delete</button>';
-   ul.append(li);
+    const ul = document.getElementById('task-list');
+    const li = document.createElement('li');
+    li.innerHTML = text + '<button>done</button>\n' +
+        '<button>delete</button>';
+    ul.append(li);
 }
-var inputText = document.getElementById('task');
-console.log(inputText);
-inputText.addEventListener('keypress', (keyboardEvent) => {
-   const keyEnter = 13;
-   if (onkeypress == keyEnter) {
-      addTask()
-   }
-})
-// function f() {
-//    document.querySelector('input', addTask(text)).addEventListener('keydown', function (e) {
-//       if (e.key === 13) {
-//          addTask("");
-//       }
-//
-//    })
 
-// }
+const inputText = document.getElementById('task');
+inputText.addEventListener('keypress', (keyboardEvent) => {
+    const keyEnter = 13;
+    const taskText = document.getElementById('task').value;
+    console.log(inputText);
+    if (keyboardEvent.keyCode == keyEnter) {
+        addTask(taskText)
+    }
+    console.log(keyboardEvent)
+})
+
+function onPageLoaded() {
+    const doneButton = document.getElementById('done');
+    const deleteButton = document.getElementById('delete');
+    doneButton.addEventListener('click', () => {
+        localStorage.setItem("todos", ul.innerHTML)
+    })
+    deleteButton.addEventListener('click', () => {
+        localStorage.removeItem("todos", ul.innerHTML)
+    })
+   
+}
+
